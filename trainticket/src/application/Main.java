06464826b1bc01e10cc6f041aa0c7b8cd0114cf7
Sarkitdas <78489797+Sarkitdas@users.noreply.github.com>
2022-06-15 00:29:33 +0600

    Add files via upload

---
+++ trainticket/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import java.io.IOException;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application 
+{
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		Parent root= FXMLLoader.load(getClass().getResource("trainticket.fxml"));
+		primaryStage.setTitle("Saikat");
+		primaryStage.setScene(new Scene(root,700,400));
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+}
